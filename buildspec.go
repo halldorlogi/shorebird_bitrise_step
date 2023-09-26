70c0349dec6c249c991627dab411a30ab062c781
Jon Holtan <jon@uptechstudio.com>
2023-09-26 10:37:05 -0700

    Initial Commit Not production ready yet.
    Initial commit to setup the step and begin testing.

---
+++ buildspec.go
@@ -0,0 +1,235 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/bitrise-io/go-steputils/output"
+	"github.com/bitrise-io/go-steputils/tools"
+	"github.com/bitrise-io/go-utils/command"
+	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-io/go-utils/sliceutil"
+	"github.com/bitrise-io/go-utils/ziputil"
+	"github.com/kballard/go-shellquote"
+	"github.com/ryanuber/go-glob"
+)
+
+type buildSpecification struct {
+	displayName          string
+	platformOutputType   OutputType
+	platformSelectors    []string
+	outputPathPatterns   []string
+	additionalParameters string
+	projectLocation      string
+}
+
+func (spec buildSpecification) exportArtifacts(artifacts []string) error {
+	deployDir := os.Getenv("BITRISE_DEPLOY_DIR")
+	switch spec.platformOutputType {
+	case OutputTypeAPK:
+		return spec.exportAndroidArtifacts(OutputTypeAPK, artifacts, deployDir)
+	case OutputTypeAppBundle:
+		return spec.exportAndroidArtifacts(OutputTypeAppBundle, artifacts, deployDir)
+	case OutputTypeIOSApp:
+		return spec.exportIOSApp(artifacts, deployDir)
+	case OutputTypeArchive:
+		return spec.exportIOSArchive(artifacts, deployDir)
+	default:
+		return fmt.Errorf("unsupported platform for exporting artifacts: %s. Supported platforms: apk, appbundle, app, archive", spec.platformOutputType)
+	}
+}
+
+func (spec buildSpecification) artifactPaths(outputPathPatterns []string, isDir bool) ([]string, error) {
+	var paths []string
+	for _, outputPathPattern := range outputPathPatterns {
+		pths, err := findPaths(spec.projectLocation, outputPathPattern, isDir)
+		if err != nil {
+			return nil, err
+		}
+		paths = append(paths, pths...)
+	}
+	return paths, nil
+}
+
+func (spec buildSpecification) exportIOSApp(artifacts []string, deployDir string) error {
+	artifact := artifacts[len(artifacts)-1]
+	fileName := filepath.Base(artifact)
+
+	if len(artifacts) > 1 {
+		log.Warnf("- Multiple artifacts found: %v, exporting %s", artifacts, artifact)
+	}
+
+	if err := ziputil.ZipDir(artifact, filepath.Join(deployDir, fileName+".zip"), false); err != nil {
+		return err
+	}
+	log.Donef("- $BITRISE_DEPLOY_DIR/" + fileName + ".zip")
+
+	if err := tools.ExportEnvironmentWithEnvman("BITRISE_APP_DIR_PATH", artifact); err != nil {
+		return err
+	}
+	log.Donef("- $BITRISE_APP_DIR_PATH: " + artifact)
+
+	return nil
+}
+
+func (spec buildSpecification) exportIOSArchive(artifacts []string, deployDir string) error {
+	artifact := artifacts[len(artifacts)-1]
+	fileName := filepath.Base(artifact)
+
+	if len(artifacts) > 1 {
+		log.Warnf("- Multiple artifacts found: %v, exporting %s", artifacts, artifact)
+	}
+
+	zipPath := filepath.Join(deployDir, fileName+".zip")
+	if err := ziputil.ZipDir(artifact, zipPath, false); err != nil {
+		return err
+	}
+	log.Donef("- $BITRISE_DEPLOY_DIR/" + fileName + ".zip")
+
+	if err := tools.ExportEnvironmentWithEnvman("BITRISE_XCARCHIVE_PATH", artifact); err != nil {
+		return err
+	}
+	log.Donef("- $BITRISE_XCARCHIVE_PATH: " + artifact)
+
+	if err := tools.ExportEnvironmentWithEnvman("BITRISE_XCARCHIVE_ZIP_PATH", zipPath); err != nil {
+		return err
+	}
+	log.Donef("- BITRISE_XCARCHIVE_ZIP_PATH: " + zipPath)
+
+	return nil
+}
+
+func (spec buildSpecification) exportAndroidArtifacts(androidOutputType OutputType, artifacts []string, deployDir string) error {
+	artifacts = filterAndroidArtifactsBy(androidOutputType, artifacts)
+
+	var singleFileOutputEnvName string
+	var multipleFileOutputEnvName string
+	switch spec.platformOutputType {
+	case "appbundle":
+		singleFileOutputEnvName = "BITRISE_AAB_PATH"
+		multipleFileOutputEnvName = "BITRISE_AAB_PATH_LIST"
+	default:
+		singleFileOutputEnvName = "BITRISE_APK_PATH"
+		multipleFileOutputEnvName = "BITRISE_APK_PATH_LIST"
+	}
+
+	var deployedFiles []string
+	for _, path := range artifacts {
+		deployedFilePath := filepath.Join(deployDir, filepath.Base(path))
+
+		if err := output.ExportOutputFile(path, deployedFilePath, singleFileOutputEnvName); err != nil {
+			return err
+		}
+		deployedFiles = append(deployedFiles, deployedFilePath)
+	}
+	if err := tools.ExportEnvironmentWithEnvman(multipleFileOutputEnvName, strings.Join(deployedFiles, "\n")); err != nil {
+		return fmt.Errorf("failed to export enviroment variable %s, error: %s", multipleFileOutputEnvName, err)
+	}
+
+	deployedSingleFile := ""
+	if len(deployedFiles) > 0 {
+		deployedSingleFile = deployedFiles[len(deployedFiles)-1]
+	}
+
+	log.Donef("- " + singleFileOutputEnvName + ": " + deployedSingleFile)
+	log.Donef("- " + multipleFileOutputEnvName + ": " + strings.Join(deployedFiles, "|"))
+	return nil
+}
+
+func filterAndroidArtifactsBy(androidOutputType OutputType, artifacts []string) []string {
+	var index int
+	for _, artifact := range artifacts {
+		switch androidOutputType {
+		case OutputTypeAPK:
+			if path.Ext(artifact) != ".apk" {
+				log.Debugf("Artifact (%s) found by output patterns, but it's not the selected output type (%s) - Skip", artifact, androidOutputType)
+				continue // drop artifact
+			}
+		case OutputTypeAppBundle:
+			if path.Ext(artifact) != ".aab" {
+				log.Debugf("Artifact (%s) found by output patterns, but it's not the selected output type (%s) - Skip", artifact, androidOutputType)
+				continue // drop artifact
+			}
+		}
+		artifacts[index] = artifact
+		index++
+	}
+	return artifacts[:index]
+}
+
+func (spec buildSpecification) buildable(platform string) bool {
+	return sliceutil.IsStringInSlice(platform, spec.platformSelectors)
+}
+
+func findPaths(location string, outputPathPattern string, dir bool) (out []string, err error) {
+	err = filepath.Walk(location, func(path string, info os.FileInfo, walkErr error) error {
+		if walkErr != nil {
+			return walkErr
+		}
+
+		if !info.IsDir() == dir || !glob.Glob(outputPathPattern, path) {
+			return nil
+		}
+
+		out = append(out, path)
+		return nil
+	})
+	if len(out) == 0 && err == nil {
+		log.Debugf("couldn't find output artifact on path: " + filepath.Join(location, outputPathPattern))
+	}
+	return
+}
+
+func (spec buildSpecification) build(params string) error {
+	paramSlice, err := shellquote.Split(params)
+	if err != nil {
+		return err
+	}
+
+	var errorWriter io.Writer = os.Stderr
+	var errBuffer bytes.Buffer
+
+	var platformCmd string
+	switch spec.platformOutputType {
+	case OutputTypeIOSApp:
+		platformCmd = "ios" // $ flutter build ios -> .app output
+	case OutputTypeArchive:
+		platformCmd = "ipa" // $ flutter build ipa -> .xcarchive output
+	default:
+		platformCmd = string(spec.platformOutputType)
+	}
+
+	if spec.platformOutputType == OutputTypeIOSApp {
+		paramSlice = append(paramSlice, "--no-codesign")
+	}
+
+	buildCmd := command.New("shorebird", append([]string{"build", platformCmd}, paramSlice...)...).SetStdout(os.Stdout)
+
+	if spec.platformOutputType == OutputTypeIOSApp || spec.platformOutputType == OutputTypeArchive {
+		buildCmd.SetStdin(strings.NewReader("a")) // if the CLI asks to input the selected identity we force it to be aborted
+		errorWriter = io.MultiWriter(os.Stderr, &errBuffer)
+	}
+
+	buildCmd.SetStderr(errorWriter)
+
+	fmt.Println()
+	log.Donef("$ %s", buildCmd.PrintableCommandArgs())
+	fmt.Println()
+
+	buildCmd.SetDir(spec.projectLocation)
+
+	err = buildCmd.Run()
+
+	if spec.platformOutputType == OutputTypeIOSApp {
+		if strings.Contains(strings.ToLower(errBuffer.String()), "code signing is required") {
+			return errCodeSign
+		}
+	}
+
+	return err
+}
---
+++ main.go
@@ -0,0 +1,255 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/bitrise-io/go-steputils/stepconf"
+	"github.com/bitrise-io/go-utils/fileutil"
+	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-io/go-utils/pathutil"
+	"github.com/bitrise-io/go-utils/sliceutil"
+	"github.com/bitrise-io/go-xcode/certificateutil"
+	shellquote "github.com/kballard/go-shellquote"
+)
+
+type OutputType string
+
+const (
+	codesignField  = "ios-signing-cert"
+	noCodesignFlag = "--no-codesign"
+
+	OutputTypeAPK       OutputType = "apk"
+	OutputTypeAppBundle OutputType = "appbundle"
+
+	OutputTypeIOSApp  OutputType = "app"     // CLI: flutter build ios
+	OutputTypeArchive OutputType = "archive" // CLI: flutter build ipa
+)
+
+var flutterConfigPath = filepath.Join(os.Getenv("HOME"), ".flutter_settings")
+var errCodeSign = errors.New("CODESIGN")
+
+type config struct {
+	ProjectLocation       string `env:"project_location,dir"`
+	Platform              string `env:"platform,opt[both,ios,android]"`
+	AdditionalBuildParams string `env:"additional_build_params"`
+	ShorebirdCiToken	  string `env:"shorebird_cli_token"`
+	DebugMode             bool   `env:"is_debug_mode,opt[true,false]"`
+	CacheLevel            string `env:"cache_level,opt[all,none]"`
+
+	IOSOutputType       OutputType `env:"ios_output_type,opt[app,archive]"`
+	IOSAdditionalParams string     `env:"ios_additional_params"`
+	IOSExportPattern    []string   `env:"ios_output_pattern,multiline"`
+	IOSCodesignIdentity string     `env:"ios_codesign_identity"`
+
+	AndroidOutputType       OutputType `env:"android_output_type,opt[apk,appbundle]"`
+	AndroidAdditionalParams string     `env:"android_additional_params"`
+	AndroidExportPattern    []string   `env:"android_output_pattern,multiline"`
+
+	// Deprecated
+	AndroidBundleExportPattern []string `env:"android_bundle_output_pattern,multiline"`
+}
+
+func failf(msg string, args ...interface{}) {
+	log.Errorf(msg, args...)
+	os.Exit(1)
+}
+
+func handleDeprecatedInputs(cfg *config) {
+	if len(cfg.AndroidBundleExportPattern) > 0 && cfg.AndroidBundleExportPattern[0] != "*build/app/outputs/bundle/*/*.aab" {
+		log.Warnf("step input 'App bundle output pattern' (android_bundle_output_pattern) is deprecated and will be removed on 20 November 2019, use 'Output (.apk, .aab) pattern' (android_output_pattern) instead!")
+		log.Printf("Using 'App bundle output pattern' (android_bundle_output_pattern) instead of 'Output (.apk, .aab) pattern' (android_output_pattern).")
+		log.Printf("If you don't want to use 'App bundle output pattern' (android_bundle_output_pattern), empty it's value.")
+
+		cfg.AndroidExportPattern = cfg.AndroidBundleExportPattern
+	}
+}
+
+func main() {
+	var cfg config
+	if err := stepconf.Parse(&cfg); err != nil {
+		failf("Process config: failed to parse input: %s", err)
+	}
+	stepconf.Print(cfg)
+	handleDeprecatedInputs(&cfg)
+	log.SetEnableDebugLog(cfg.DebugMode)
+
+	projectLocationAbs, err := filepath.Abs(cfg.ProjectLocation)
+	if err != nil {
+		failf("Process config: failed to get absolute project path of %s: %s", cfg.ProjectLocation, err)
+	}
+
+	exist, err := pathutil.IsDirExists(projectLocationAbs)
+	if err != nil {
+		failf("Process config: failed to check if project path exists: %s", err)
+	} else if !exist {
+		failf("Process config: project path does not exist")
+	}
+
+	if cfg.Platform == "ios" || cfg.Platform == "both" {
+		fmt.Println()
+		log.Infof("iOS Codesign settings")
+
+		iosParams, err := shellquote.Split(cfg.IOSAdditionalParams)
+		if err != nil {
+			failf("Process config: failed to parse iOS additional parameters: %s", err)
+		}
+		if sliceutil.IsStringInSlice(noCodesignFlag, iosParams) {
+			log.Printf(" - Skipping codesign preparation, %s parameter set", noCodesignFlag)
+			goto build
+		}
+		if cfg.IOSOutputType == OutputTypeIOSApp {
+			log.Printf(" - Skipping codesign preparation because output type is iOS app, not xcarchive")
+			goto build
+		}
+
+		log.Printf(" Installed codesign identities:")
+		installedCertificates, err := certificateutil.InstalledCodesigningCertificateNames()
+		if err != nil {
+			failf("Run: failed to fetch installed codesign identities: %s", err)
+		}
+		for _, identity := range installedCertificates {
+			log.Printf(" - %s", identity)
+		}
+
+		if len(installedCertificates) == 0 {
+			failf("Run: no codesign identities installed")
+		}
+
+		var flutterSettings map[string]string
+		flutterSettingsExists, err := pathutil.IsPathExists(flutterConfigPath)
+		if err != nil {
+			failf("Run: failed to check if %s exists: %s", flutterConfigPath, err)
+		}
+		if flutterSettingsExists {
+			flutterSettingsContent, err := fileutil.ReadBytesFromFile(flutterConfigPath)
+			if err != nil {
+				failf("Run: error while reading %s: %s", flutterConfigPath, err)
+			}
+			if err := json.Unmarshal(flutterSettingsContent, &flutterSettings); err != nil {
+				failf("Run: failed to parse .flutter_settings file: %s", err)
+			}
+		} else {
+			flutterSettings = map[string]string{}
+		}
+
+		if cfg.IOSCodesignIdentity != "" {
+			log.Warnf(" Override codesign identity:")
+			log.Printf(" - Store: %s", cfg.IOSCodesignIdentity)
+			if !sliceutil.IsStringInSlice(cfg.IOSCodesignIdentity, installedCertificates) {
+				failf("Process config: the selected identity \"%s\" is not installed on the system", cfg.IOSCodesignIdentity)
+			}
+			flutterSettings[codesignField] = cfg.IOSCodesignIdentity
+			newSettingsContent, err := json.MarshalIndent(flutterSettings, "", " ")
+			if err != nil {
+				failf("Run: failed to parse .flutter_settings file: %s", err)
+			}
+			if err := fileutil.WriteBytesToFile(flutterConfigPath, newSettingsContent); err != nil {
+				failf("Run: error while writing .flutter_settings file: %s", err)
+			}
+			log.Donef(" - Done")
+			goto build
+		}
+
+		log.Printf(" Stored Flutter codesign settings:")
+		storedIdentity, ok := flutterSettings["ios-signing-cert"]
+		if !ok {
+			log.Printf(" - No codesign identity set")
+		} else {
+			log.Printf(" - %s", storedIdentity)
+			if !sliceutil.IsStringInSlice(storedIdentity, installedCertificates) {
+				failf("Process config: identity \"%s\" is not installed on the system", storedIdentity)
+			}
+		}
+	}
+
+build:
+
+	buildSpecifications := []buildSpecification{
+		{
+			displayName:          "iOS app",
+			platformOutputType:   cfg.IOSOutputType,
+			platformSelectors:    []string{"both", "ios"},
+			outputPathPatterns:   cfg.IOSExportPattern,
+			additionalParameters: cfg.AdditionalBuildParams + " " + cfg.IOSAdditionalParams,
+		},
+		{
+			displayName:          "Android app",
+			platformOutputType:   cfg.AndroidOutputType,
+			platformSelectors:    []string{"both", "android"},
+			outputPathPatterns:   cfg.AndroidExportPattern,
+			additionalParameters: cfg.AdditionalBuildParams + " " + cfg.AndroidAdditionalParams,
+		},
+	}
+
+	for _, spec := range buildSpecifications {
+		if !spec.buildable(cfg.Platform) {
+			continue
+		}
+
+		spec.projectLocation = projectLocationAbs
+
+		fmt.Println()
+		log.Infof("Build " + spec.displayName)
+		if err := spec.build(spec.additionalParameters); err != nil {
+			if err == errCodeSign {
+				if cfg.IOSCodesignIdentity != "" {
+					log.Warnf("Invalid codesign identity is selected, choose the appropriate identity in the step's [iOS Platform Configs>Codesign Identity] input field.")
+				} else {
+					log.Warnf("You have multiple codesign identity installed, select the one you want to use and set its name in the [iOS Platform Configs>Codesign Identity] input field.")
+				}
+			}
+
+			failf("Run: failed to build %s: %s", spec.displayName, err)
+		}
+
+		fmt.Println()
+		log.Infof("Export " + spec.displayName + " artifact")
+
+		var artifacts []string
+		var err error
+
+		if spec.platformOutputType == OutputTypeAPK || spec.platformOutputType == OutputTypeAppBundle {
+			artifacts, err = spec.artifactPaths(spec.outputPathPatterns, false)
+		} else {
+			artifacts, err = spec.artifactPaths(spec.outputPathPatterns, true)
+		}
+
+		if err != nil {
+			failf("Export outputs: failed to find artifacts: %s", err)
+		}
+
+		if len(artifacts) < 1 {
+			failf(`Export outputs: artifact path pattern (%s) did not match any artifacts on the path (%s).
+Check that 'iOS/Android Output Pattern' and 'Project Location' is correct.`, spec.outputPathPatterns, spec.projectLocation)
+		}
+
+		if err := spec.exportArtifacts(artifacts); err != nil {
+			failf("Export outputs: failed to export %s artifacts: %s", spec.displayName, err)
+		}
+	}
+
+	if cfg.CacheLevel == "all" {
+		fmt.Println()
+		log.Infof("Collecting cache")
+
+		if err := cacheCocoapodsDeps(projectLocationAbs); err != nil {
+			log.Warnf("Failed to collect cocoapods cache, error: %s", err)
+		}
+
+		if err := cacheCarthageDeps(projectLocationAbs); err != nil {
+			log.Warnf("Failed to collect carthage cache, error: %s", err)
+		}
+
+		if err := cacheAndroidDeps(projectLocationAbs); err != nil {
+			log.Warnf("Failed to collect android cache, error: %s", err)
+		}
+
+		if err := cacheFlutterDeps(projectLocationAbs); err != nil {
+			log.Warnf("Failed to collect flutter cache, error: %s", err)
+		}
+	}
+}
